9cdbcfb47c82d4d32c66e7e4cb0a77f41d357ae2
Edwin Kofler <edwin@kofler.dev>
2022-07-17 03:00:56 -0700

    feat: Write files for each language

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # Fox Lang
 
 Write once, transpile everywhere.
+
+> Ease multi-language authoring by limiting interfaces to those that are both pure and commonplace. Non-traditional interfaces are trivially hand-written, composing the transpiled output
+
+STATUS: EARLY DEVELOPMENT
+
+## Use when
+
+- FFI doesn't cut it
+- Performance overhead is negligible
+- Code is rather generic (in terms of types and their behavior)
+
+## Usage
+
+Don't use yet.
+
+1. A custom GitHub organization for holding the transpiled code
+2. A custom GitHub bot for making commits (so your main account doesn't rack up hundreds of commits in a single day)
+
+## Examples
+
+- [./test.fox](./test.fox)
+- [fox-xdg-base-dir](https://github.com/hyperupcall/fox-xdg-base-dir)
--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+import * as path from 'https://deno.land/std@0.148.0/path/mod.ts'
+import * as fs from 'https://deno.land/std@0.148.0/fs/mod.ts'
+
 // To get the ball rolling
 
 const util = {
@@ -60,7 +63,10 @@ type Context = {
 
 await main()
 async function main() {
-	const content = await Deno.readTextFile('test.fox')
+	const fileName = Deno.args[0] ? Deno.args[0] : 'test.fox'
+	const testMode = fileName === 'test.fox'
+
+	const content = await Deno.readTextFile(fileName)
 	const ctx: Context = {
 		text: content,
 		position: 0,
@@ -97,6 +103,45 @@ async function main() {
 	}
 
 	console.log(ast)
+	if (testMode) return
+
+	const languages: LanguageName[] = []
+	for (const node of ast) {
+		if (node.type === 'language') {
+			if (node.data?.language) {
+				languages.push(node.data.language as LanguageName)
+			}
+		}
+	}
+
+	for (const language of languages) {
+		let fileContent = ''
+
+		for (const node of ast) {
+			if (node.type === 'language') {
+				if (node.data?.language == language) {
+					fileContent += (node.data?.body as string)
+						.split('\n')
+						.map((line) => {
+							return line[0] == '\t' ? line.slice(1) : line
+						})
+						.join('\n')
+				}
+			} else if (node.type === 'empty') {
+				fileContent += node.content
+			}
+		}
+
+		const dir = path.join('languages', language)
+		await Deno.mkdir(dir, { recursive: true })
+		await Deno.writeTextFile(
+			path.join(
+				dir,
+				`${path.basename(fileName).split('.')[0]}.${getExt(language)}`
+			),
+			fileContent
+		)
+	}
 }
 
 function eatFunctionBlock(ctx: Context, ast: Ast): Node {
@@ -152,4 +197,125 @@ function eatAll(ctx: Context): void {
 function die(msg: string): never {
 	console.error(`Error: ${msg}`)
 	Deno.exit(1)
+}
+
+type LanguageName =
+	| 'python'
+	| 'c'
+	| 'c-header'
+	| 'java'
+	| 'cpp'
+	| 'cpp-header'
+	| 'c#'
+	| 'javascript'
+	| 'swift'
+	| 'php'
+	| 'go'
+	| 'delphi'
+	| 'ruby'
+	| 'objective-c'
+	| 'perl'
+	| 'fortran'
+	| 'r'
+	| 'lua'
+	| 'cobol'
+	| 'julia'
+	| 'd'
+	| 'rust'
+	| 'ada'
+	| 'dart'
+	| 'haskell'
+	| 'scala'
+	| 'kotlin'
+	| 'typescript'
+	| 'awk'
+	| 'powershell'
+	| 'bash'
+	| 'nim'
+	| 'crystal'
+	| 'odin'
+	| 'hare'
+	| 'elm'
+	| 'v'
+
+function getExt(languageName: LanguageName) {
+	switch (languageName.trim()) {
+		case 'python':
+			return 'py'
+		case 'c':
+			return 'c'
+		case 'c-header':
+			return 'h'
+		case 'java':
+			return 'java'
+		case 'cpp':
+			return 'cpp'
+		case 'cpp-header':
+			return 'hpp'
+		case 'cs':
+			return 'cs'
+		case 'javascript':
+			return 'js'
+		case 'swift':
+		case 'php':
+		case 'go':
+			return languageName
+		case 'delphi':
+			return 'dpr'
+		case 'ruby':
+			return 'rb'
+		case 'objective-c':
+			return 'm'
+		case 'perl':
+			return 'perl'
+		case 'fortran':
+			return 'f90'
+		case 'r':
+			return 'r'
+		case 'lua':
+			return 'lua'
+		case 'cobol':
+			return 'cbl'
+		case 'julia':
+			return 'jl'
+		case 'd':
+			return 'd'
+		case 'rust':
+			return 'rs'
+		case 'ada':
+			return 'ads'
+		case 'dart':
+			return 'dart'
+		case 'haskell':
+			return 'hs'
+		case 'scala':
+			return 'sc'
+		case 'kotlin':
+			return 'kt'
+		case 'deno':
+			return 'ts'
+		case 'nodejs':
+			return 'ts'
+		case 'awk':
+			return 'awk'
+		case 'powershell':
+			return 'awk'
+		case 'bash':
+			return 'sh'
+		case 'nim':
+			return 'nim'
+		case 'crystal':
+			return 'cr'
+		case 'odin':
+			return 'odin'
+		case 'hare':
+			return 'fa'
+		case 'elm':
+			return 'elm'
+		case 'v':
+			return 'v'
+		default:
+			console.error(`Unexpected language: ${languageName}`)
+			Deno.exit(1)
+	}
 }
